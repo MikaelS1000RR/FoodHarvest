efb467e1f4984b2701206ea1443a2a56c08b669c
Qiling <professorqtaku@gmail.com>
2021-09-20 11:53:15 +0200

    added components
    Co-Authored-By: Denise <72066356+DeniiseB@users.noreply.github.com>
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>
    Co-Authored-By: MikaelS1000RR <80256274+MikaelS1000RR@users.noreply.github.com>
    Co-Authored-By: Anna Tch <72065138+anna636@users.noreply.github.com>
    Co-Authored-By: nicolebradleey <72066187+nicolebradleey@users.noreply.github.com>
    Co-Authored-By: Johanna Andersson <72018736+snabelA2000@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
       </header>
       <main>
 
