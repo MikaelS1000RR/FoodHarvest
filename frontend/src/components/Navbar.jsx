c0f0a22c52b6b132bda486fb42db1f2da7bce98b
Jonte111 <jonathan.pe@telia.com>
2021-09-20 15:35:04 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,14 +16,48 @@ import {
 
 const Example = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
 
-  const toggle = () => setIsOpen(!isOpen);
+
+  // const toggle = () => setIsOpen(!isOpen);
+  // const toggle2 = () => setIsOpen2(!isOpen2);
+
 
   return (
     <div>
       <Navbar color="light" light expand="md">
+        <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
         <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
+        <NavbarToggler onClick={() => setIsOpen2(!isOpen2)} />
+
+        <Collapse isOpen2={isOpen2} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
