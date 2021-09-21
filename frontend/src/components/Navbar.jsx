b2ce251d292d35b129baec79112cf93905783097
Jonte111 <jonathan.pe@telia.com>
2021-09-21 15:13:22 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -48,31 +48,31 @@ const Example = (props) => {
         <NavbarBrand className="text-white" href="/">FoodHarvest</NavbarBrand>
         <NavbarToggler onClick={toggleHamburgerMenu} />
         <Collapse isOpen={hamburgerMenu} navbar>
-          <Nav className="mr-auto" navbar>
+          <Nav className="mr-auto" navbar className="text-white">
             <div>!!!Inte inloggad</div>
             {/* not logged in */}
             <NavItem>
-              <NavLink href="/catagories/">Kategorier</NavLink>
+              <NavLink href="/catagories/" className="text-white">Kategorier</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/login/">Logga in</NavLink>
+              <NavLink href="/login/" className="text-white">Logga in</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/register/">Registrera dig</NavLink>
+              <NavLink href="/register/" className="text-white">Registrera dig</NavLink>
             </NavItem>
             {/* Logged in */}
             <div>!!!När inloggad</div>
             <NavItem>
-              <NavLink href="/myProductListPage/">Inköpslistor</NavLink>
+              <NavLink href="/myProductListPage/" className="text-white">Inköpslistor</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/catagories/">Kategorier</NavLink>
+              <NavLink href="/catagories/" className="text-white">Kategorier</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/myProfile/">Mina sidor</NavLink>
+              <NavLink href="/myProfile/" className="text-white">Mina sidor</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/">Logga ut</NavLink>
+              <NavLink href="/" className="text-white">Logga ut</NavLink>
             </NavItem>
           </Nav>
         </Collapse>
