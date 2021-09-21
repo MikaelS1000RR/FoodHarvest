f3a410f8979222bcc36199e84fd7b9287beff93b
Jonte111 <jonathan.pe@telia.com>
2021-09-21 14:27:01 +0200

    Restarted after more testing.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import {
   Collapse,
   Navbar,
@@ -12,89 +11,152 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  Dropdown,
   NavbarText
 } from 'reactstrap';
 
 const Example = (props) => {
-  // const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  // const toggle = () => setIsOpen(!isOpen);
-
-  // -----------------------------------------
-
-  const [navDropdownOpen, setDropdownOpen] = useState(false);
-  const toggleNavDropdown = () => {
-    setDropdownOpen(!navDropdownOpen);
-  }
-
-  const [chooseListDropDown, setChooseListDropDown] = useState(false);
-  const toggleChooseListDropDown = () => {
-    setChooseListDropDown(!chooseListDropDown);
-  }
-
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div>
-      <Navbar color="primary" light expand="md">
-        {/* <NavbarToggler onClick={toggle} /> */}
-        {/* <Collapse isOpen={isOpen} navbar> */}
-        <Nav className="mr-auto" navbar>
-          <Dropdown isOpen={navDropdownOpen} toggle={toggleNavDropdown}>
-            <DropdownToggle caret>
-              ≡
-            </DropdownToggle>
-            <DropdownMenu>
-              <div>Inte inloggad</div>
-              {/* not logged in */}
-              <NavItem>
-                <NavLink href="/catagories/">Kategorier</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/login/">Logga in</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/register/">Registrera dig</NavLink>
-              </NavItem>
-              {/* Logged in */}
-              <div>När inloggad</div>
-              <NavItem>
-                <NavLink href="/myProductListPage/">Inköpslistor</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/catagories/">Kategorier</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/myProfile/">Mina sidor</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/">Logga ut</NavLink>
-              </NavItem>
-            </DropdownMenu>
-          </Dropdown>
-        </Nav>
-        {/* </Collapse> */}
-        <NavbarBrand href="/">FoodHarvest</NavbarBrand>
-        {/* Second dropdown */}
-        <Nav className="mr-auto" navbar>
-          <Dropdown isOpen={chooseListDropDown} toggle={toggleChooseListDropDown}>
-            <DropdownToggle caret>
-              ❤️ Välj lista
-            </DropdownToggle>
-            <DropdownMenu>
-              <div>test</div>
-              <div>test</div>
-              <div>test</div>
-              <div>Lägg till en lista +</div>
-            </DropdownMenu>
-          </Dropdown>
-        </Nav>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
       </Navbar>
     </div>
   );
 }
 
 export default Example;
+
+
+// import React, { useState } from 'react';
+
+// import {
+//   Collapse,
+//   Navbar,
+//   NavbarToggler,
+//   NavbarBrand,
+//   Nav,
+//   NavItem,
+//   NavLink,
+//   UncontrolledDropdown,
+//   DropdownToggle,
+//   DropdownMenu,
+//   DropdownItem,
+//   Dropdown,
+//   NavbarText
+// } from 'reactstrap';
+
+// const Example = (props) => {
+//   // const [isOpen, setIsOpen] = useState(false);
+
+//   // const toggle = () => setIsOpen(!isOpen);
+
+//   // -----------------------------------------
+
+//   const [navDropdownOpen, setDropdownOpen] = useState(false);
+//   const toggleNavDropdown = () => {
+//     setDropdownOpen(!navDropdownOpen);
+//   }
+
+//   const [chooseListDropDown, setChooseListDropDown] = useState(false);
+//   const toggleChooseListDropDown = () => {
+//     setChooseListDropDown(!chooseListDropDown);
+//   }
+
+
+//   return (
+//     <div>
+//       <Navbar color="primary" light expand="md">
+//         {/* <NavbarToggler onClick={toggle} /> */}
+//         {/* <Collapse isOpen={isOpen} navbar> */}
+//         <Nav className="mr-auto" navbar>
+//           <Dropdown isOpen={navDropdownOpen} toggle={toggleNavDropdown}>
+//             <DropdownToggle caret>
+//               ≡
+//             </DropdownToggle>
+//             <DropdownMenu>
+//               <div>Inte inloggad</div>
+//               {/* not logged in */}
+//               <NavItem>
+//                 <NavLink href="/catagories/">Kategorier</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href="/login/">Logga in</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href="/register/">Registrera dig</NavLink>
+//               </NavItem>
+//               {/* Logged in */}
+//               <div>När inloggad</div>
+//               <NavItem>
+//                 <NavLink href="/myProductListPage/">Inköpslistor</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href="/catagories/">Kategorier</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href="/myProfile/">Mina sidor</NavLink>
+//               </NavItem>
+//               <NavItem>
+//                 <NavLink href="/">Logga ut</NavLink>
+//               </NavItem>
+//             </DropdownMenu>
+//           </Dropdown>
+//         </Nav>
+//         {/* </Collapse> */}
+//         <NavbarBrand href="/">FoodHarvest</NavbarBrand>
+//         {/* Second dropdown */}
+//         <Nav className="mr-auto" navbar>
+//           <Dropdown isOpen={chooseListDropDown} toggle={toggleChooseListDropDown}>
+//             <DropdownToggle caret>
+//               ❤️ Välj lista
+//             </DropdownToggle>
+//             <DropdownMenu>
+//               <div>test</div>
+//               <div>test</div>
+//               <div>test</div>
+//               <div>Lägg till en lista +</div>
+//             </DropdownMenu>
+//           </Dropdown>
+//         </Nav>
+//       </Navbar>
+//     </div>
+//   );
+// }
+
+// export default Example;
 
 
 
