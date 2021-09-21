3847242debe11a1c5255391a71f360e304bb4c8d
snabelA00 <ja000411@gmail.com>
2021-09-21 10:28:30 +0200

    $ npm install firebase-admin --save

--- frontend/src/database/firebase.js
+++ frontend/src/database/firebase.js
@@ -2,16 +2,6 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
 
-
-var admin = require("firebase-admin");
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-
-
 // Initialize Firebase
 
 var config = {
