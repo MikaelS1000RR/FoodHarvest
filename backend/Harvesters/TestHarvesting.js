e3ddf8d7a0b7b79c597c743768488c036e8baacf
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-09-22 14:40:22 +0200

    Took all products from Willys store
    Co-Authored-By: MikaelS1000RR <80256274+MikaelS1000RR@users.noreply.github.com>

--- backend/Harvesters/TestHarvesting.js
+++ backend/Harvesters/TestHarvesting.js
@@ -1,22 +1,29 @@
-
-import { WillysHarvester } from './WillysHarvester.js'
-
+import fs from 'fs'
+import { WillysHarvester } from "./WillysHarvester.js";
 
 export class TestHarvesting {
   static async test() {
     // write to file for now (goal: write to DB instead)
-  
 
-    // let categories = await WillysHarvester.getProducts();
-    let frystFagel = await WillysHarvester.getProducts(
-      "Kott-chark-och-fagel/Fagel/Fryst-fagel"
-      // note we can also fetch all Kott-chark-och-fagel at ONCE!
-      // 'Kott-chark-och-fagel'
-    );
+    function writeToFile(fileName, data) {
+      fs.writeFileSync(fileName, JSON.stringify(data, null, "  "), "utf-8");
+    }
+
+    let rawData = await WillysHarvester.getCategories();
+   
+    let categories = rawData.children; //children of raw data aka all categories inside of array where each category is an object
 
 
-    
-    console.log("Test harvesting and scrubbing done!" + ' ' + JSON.stringify(frystFagel));
+    let allProductsOfWillys=[]
+    for (var i = 0; i < categories.length; i++) {
+
+      let category = categories[i]; //Getting each category
+      let productsOfCategory = await WillysHarvester.getProducts(category.url); //Sending url of each basic category to getProducts method
+      allProductsOfWillys.push(productsOfCategory) //Pushing all products of all categories into one big array
+
+    }
+
+  writeToFile("willys-all-products.json", allProductsOfWillys);
+  }
   }
 
-}
--- backend/Harvesters/WillysHarvester.js
+++ backend/Harvesters/WillysHarvester.js
@@ -4,19 +4,25 @@ export class WillysHarvester {
   static bustCache() {
     return "?avoidCache=" + (Math.random() + "").split(".")[1];
   }
+
   static async getCategories() {
-    let raw = await fetch('https://www.willys.se/leftMenu/categorytree'
-      + this.bustCache());
+    let raw = await fetch(
+      "https://www.willys.se/leftMenu/categorytree" + this.bustCache()
+    );
     return await raw.json();
   }
 
   static async getProducts(categoryURL) {
-    let raw = await fetch('https://www.willys.se/c/'
-      + categoryURL + this.bustCache() + '&size=20');
+    let raw = await fetch(
+      "https://www.willys.se/c/" +
+        categoryURL +
+        this.bustCache() +
+        "&size=10000"
+    );
     return (await raw.json()).results;
   }
 
 
-
-
+  // now loop basic categories and getProducts for each category...
+  // how would you write this?
 }
