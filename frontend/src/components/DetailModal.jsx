b32efccabbd5dade69e1df5807d994b2d3d7b280
nicolebradleey <nicole_bradley94@hotmail.com>
2021-09-22 16:08:34 +0200

    Modal fix
    Small fixes, not finished

--- frontend/src/components/DetailModal.jsx
+++ frontend/src/components/DetailModal.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 
-const DetailModal = (open) => {
-  if (!open) return null
-  
-return(
-    <div> Hello modal</div >
-    )
-  
-}
+function DetailModal ({closeModal}) {
+
+  return (
+    <div className="modalBackground" style={styles.container}>
+      <div className="modalContainer" style={styles.secondContainer}>
+        <button onClick={()=> closeModal(false)}>Close</button>
+        <div className="title">
+          <h4>Product details</h4>
+          <div className="body">
+            <p>jsankjasnkdj alksndlkn lklkkln</p>
+           </div>
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default DetailModal;
 
 
 const styles = {
-  
-}
+  container: {
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'scroll'
+  },
+  secondContainer: {
+    width: '500px',
+    height: '500px',
+    borderRadius: '12px',
+    backgroundColor: 'grey',
+    boxShadow: 'black',
+    
+
+
+  }
+};
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,26 @@
 import ProductCard from "../components/Home/ProductCard";
 import data from '../testData.js' // will be removed when real data is fetched
+import DetailModal from "../components/DetailModal";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="container" style={styles.container}>
       <div className="row gy-3">
-        {data.products.map(p =>
+        {data.products.map((p) => (
           <ProductCard
             product={p}
             classNames={"col-6 col-sm-4 col-md-3 col-lg-2"}
-            buttonText="Lägg till" />)
-        }
+            buttonText="Lägg till"
+          />
+        ))}
       </div>
+      <button className="openModal" onClick={()=> setIsOpen(true)}>Show modal</button>
+      {isOpen && <DetailModal closeModal={setIsOpen} />}
     </div>
-   );
+  );
 }
  
 export default Home;
