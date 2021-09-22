8200ce5b4a48e49446e50f94df38be2c9cecdab6
Qiling <professorqtaku@gmail.com>
2021-09-22 13:05:29 +0200

    add toggle login modal in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { LoginModalContext } from "../contexts/LoginModalContextProvider";
+
 import {
+  Button,
   Collapse,
   Navbar,
   NavbarToggler,
@@ -16,6 +19,8 @@ import {
 } from 'reactstrap';
 
 const Example = (props) => {
+  const { toggleLoginModal } = useContext(LoginModalContext)
+
   const [hamburgerMenu, setHamburgerMenu] = useState(false);
   const toggleHamburgerMenu = () => setHamburgerMenu(!hamburgerMenu);
 
@@ -27,52 +32,70 @@ const Example = (props) => {
   return (
     <div>
       <Navbar color="primary" light expand="md">
-
-
         <Nav className="mr-auto" navbar>
-           <Dropdown isOpen={chooseListDropDown} toggle={toggleChooseListDropDown}>
-             <DropdownToggle color="warning" caret>
-               ❤️ Välj lista
-             </DropdownToggle>
-             <DropdownMenu>
-               <div>Grönsaker</div>
-               <div>test</div>
-               <div>test</div>
-               <div>Lägg till en lista +</div>
-             </DropdownMenu>
-           </Dropdown>
-         </Nav>
+          <Dropdown
+            isOpen={chooseListDropDown}
+            toggle={toggleChooseListDropDown}
+          >
+            <DropdownToggle color="warning" caret>
+              ❤️ Välj lista
+            </DropdownToggle>
+            <DropdownMenu>
+              <div>Grönsaker</div>
+              <div>test</div>
+              <div>test</div>
+              <div>Lägg till en lista +</div>
+            </DropdownMenu>
+          </Dropdown>
+        </Nav>
 
-
-        
-        <NavbarBrand className="text-white" href="/">FoodHarvest</NavbarBrand>
+        <NavbarBrand className="text-white" href="/">
+          FoodHarvest
+        </NavbarBrand>
         <NavbarToggler onClick={toggleHamburgerMenu} />
         <Collapse isOpen={hamburgerMenu} navbar>
           <Nav className="mr-auto" navbar className="text-white">
             <div>!!!Inte inloggad</div>
             {/* not logged in */}
             <NavItem>
-              <NavLink href="/catagories/" className="text-white">Kategorier</NavLink>
+              <NavLink href="/catagories/" className="text-white">
+                Kategorier
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/login/" className="text-white">Logga in</NavLink>
+              <Button
+                className="text-white bg-transparent btn-outline-primary"
+                onClick={toggleLoginModal}
+              >
+                Logga in
+              </Button>
             </NavItem>
             <NavItem>
-              <NavLink href="/register/" className="text-white">Registrera dig</NavLink>
+              <NavLink href="/register/" className="text-white">
+                Registrera dig
+              </NavLink>
             </NavItem>
             {/* Logged in */}
             <div>!!!När inloggad</div>
             <NavItem>
-              <NavLink href="/myProductListPage/" className="text-white">Inköpslistor</NavLink>
+              <NavLink href="/myProductListPage/" className="text-white">
+                Inköpslistor
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/catagories/" className="text-white">Kategorier</NavLink>
+              <NavLink href="/catagories/" className="text-white">
+                Kategorier
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/myProfile/" className="text-white">Mina sidor</NavLink>
+              <NavLink href="/myProfile/" className="text-white">
+                Mina sidor
+              </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/" className="text-white">Logga ut</NavLink>
+              <NavLink href="/" className="text-white">
+                Logga ut
+              </NavLink>
             </NavItem>
           </Nav>
         </Collapse>
