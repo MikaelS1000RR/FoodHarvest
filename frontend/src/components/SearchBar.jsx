63191e2df5615d07d54e5407eb201a9eeccf73f0
Jonte111 <jonathan.pe@telia.com>
2021-09-23 09:22:04 +0200

    Fixed css regarding the searchBar.

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,13 +3,13 @@ import { Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const SearchBar = () => {
   return (
-    <Form>
+    <Form style={styles.form}>
       <FormGroup>
         <Input
           type="search"
           name="search"
           id="exampleSearch"
-          placeholder="Sök efter mat"
+          placeholder="🔎 Sök efter mat"
         />
       </FormGroup>
     </Form>
@@ -18,3 +18,11 @@ const SearchBar = () => {
 }
  
 export default SearchBar;
+
+const styles = {
+  form: {
+    padding: "10px",
+    display: "flex",
+    justifyContent: "center"
+  }
+}
