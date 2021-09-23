0e392563c88ac141de06ee6ac817ac3f030a35ad
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-09-23 11:27:57 +0200

    Hot fix
    Co-Authored-By: MikaelS1000RR <80256274+MikaelS1000RR@users.noreply.github.com>

--- backend/Scrubbers/WillysScrubber.js
+++ backend/Scrubbers/WillysScrubber.js
@@ -4,27 +4,6 @@ import { WillysHarvester } from '../Harvesters/WillysHarvester.js'
 
 export class WillysScrubber extends Scrubber {
   static translateSchema = {
-    // productName: x => x.name,
-    // brand: x => x.manufacturer,
-    // imageUrl: x => x.image && x.image.url,
-    // price: x => x.price,
-    // unitVolume: x => parseFloat(x.displayVolume.replace(/,/, '.')),
-    // unitMeasurement: x => x.displayVolume.replace(/[0-9\.]/g, ''),
-    // comparePrice: x => parseFloat(x.comparePrice.replace(/,/, '.')),
-    // compareMeasurement: x => x.comparePriceUnit,
-    // inStock: x => !x.outOfStock,
-    // frozen: x => x.labels.includes('frozen'),
-    // ecological: x => x.labels.includes('ecological'),
-    // Swedish: x => x.labels.includes('swedish_flag'),
-    // countryOfOrigin: async x => {
-    //   // Seems we need detailed product info for this...
-    //   // (one fetch per product - lots of extra time :( )
-    //   // maybe ask productOwner if Swedish/non Swedish enough?
-    //   let rawData = await fetch('https://www.willys.se/axfood/rest/p/' + x.code);
-    //   let data = await rawData.json();
-    //   return data.originCountry || data.tradeItemCountryOfOrigin
-    // }
-
     productName: (x) => x.name,
     price: (x) => x.price,
     quantity: (x) => x.displayVolume,
