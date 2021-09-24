f717318d171ebeb30dc78e05084956ed81e4c246
Jonte111 <jonathan.pe@telia.com>
2021-09-24 10:53:27 +0200

    Changed variable names.
    Co-Authored-By: Qiling <72065263+professorqtaku@users.noreply.github.com>

---
+++ frontend/src/contexts/ProductContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useContext, useEffect } from "react";
+import firestore from '../database_config/firestore'
+
+const ProductContext = createContext();
+
+export const useCategory = () => {
+  return useContext(ProductContext);
+};
+
+const ProductProvider = (props) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = () => {
+    firestore.collection('products').onSnapshot(
+      (snapshot) => {
+        const docs = [];
+        snapshot.forEach((doc) => docs.push({ id: doc.id, ...doc.data() }))
+        setProducts(docs)
+      }
+    )
+  };
+
+  const values = {
+    products,
+    fetchProducts
+  };
+
+  return (
+    <ProductContext.Provider value={values}>
+      {props.children}
+    </ProductContext.Provider>
+  );
+};
+
+export default ProductProvider;
