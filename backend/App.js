f88946125a42171446adb301119fc31114f7f45b
Denise <72066356+DeniiseB@users.noreply.github.com>
2021-09-28 15:57:09 +0200

    hemkop harvesting complete
    Co-Authored-By: nicolebradleey <72066187+nicolebradleey@users.noreply.github.com>

--- backend/App.js
+++ backend/App.js
@@ -2,11 +2,13 @@
 import express from "express";
 import path from "path";
 import { TestHarvesting } from "./Harvesters/TestHarvesting.js";
+import { HemkopTest } from "./Harvesters/HemkopTest.js";
 
 const app = express();
 
 app.use(express.json());
 
-TestHarvesting.test();
+// HemkopTest.test();
+// TestHarvesting.test();
 
 app.listen(3000, () => console.log("Listening on port 3000"));
--- backend/Harvesters/HemkopHarvester.js
+++ backend/Harvesters/HemkopHarvester.js
@@ -1,6 +1,5 @@
 import fetch from "node-fetch";
-import { FirebaseHandler } from "../FirebaseHandler";
-import { Category } from "../Models/Category";
+import { FirebaseHandler } from '../FirebaseHandler.js'
 
 export class HemkopHarvester {
   static bustCache() {
@@ -21,17 +20,61 @@ export class HemkopHarvester {
     return (await raw.json()).results;
   }
 
+
   static async getAllProducts(hemkopCategories) {
-    let hemkopProducts = [];
+    let hemkopProductsRefinedCat = [];
     let foodHarvesterCategories = await FirebaseHandler.getCategories();
 
     for (let i = 0; i < hemkopCategories.length; i++) {
       let hemkopCategory = hemkopCategories[i];
-      let hemkopCategoryProducts = await this.getProductsByCategory(
-        hemkopCategory.url
-      );
+      let hemkopProducts = await this.getProductsByCategory(hemkopCategory.url);
 
-      // continue to change each products category
+      // for (let i = 0; i < hemkopProducts.length; i++)
+      // *************temp looping 3 products in each category**************
+      for (let i = 0; i < 3; i++) {
+        hemkopProducts[i].category = this.refineCategory(
+          hemkopCategory.title,
+          foodHarvesterCategories
+        );
+
+        console.log("Hemkop category: ", hemkopCategory.title);
+        hemkopProductsRefinedCat.push(hemkopProducts[i]);
+      }
+    }
+
+    console.log("Harvesting of Hemkop is done!");
+    return hemkopProductsRefinedCat;
+  }
+
+
+  static refineCategory(storeCategoryName, dbCategories) {
+
+    // Creating an array of single words for a Hemkop category
+    let wordArr = storeCategoryName.split("&").join().split(",")
+    let wordArr2 = [];
+    for (let word of wordArr) {
+      let wordCopy = word.trim()
+      wordArr2.push(wordCopy)
+    }
+
+
+    let foundCategory = false; 
+     
+    // console.log("Hemkop Category: ", storeCategoryName, " wordArr: ", wordArr2 );
+
+    for (let i = 0; i < dbCategories.length; i++) {
+      for (let j = 0; j < wordArr2.length; j++) {
+
+        //If db category name includes any word from Hemkop category, change Hemkop category to db category
+        if (dbCategories[i].name.includes(wordArr2[j])) {
+          foundCategory = true;
+          return dbCategories[i];
+        }
+      }
+    }
+
+    if (!foundCategory) {
+      return dbCategories.find((category) => category.name === "Övrigt");
     }
   }
 }
