d910b992a5d39e50603c22d8268f01844f4dc9fb
Qiling <professorqtaku@gmail.com>
2021-09-29 12:55:27 +0200

    add backup to frontend

--- frontend/src/database_config/firestore.js
+++ frontend/src/database_config/firestore.js
@@ -1,18 +1,30 @@
-import firebase from 'firebase/compat/app'
-import "firebase/compat/auth"
-import "firebase/compat/firestore"
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 // Initialize Firebase
-let config = {
+const config = {
   apiKey: "AIzaSyAq_1HKaT-Zh2isVQU_wwX-IYcmy26bKmo",
   authDomain: "food-harvest-firestore.firebaseapp.com",
   projectId: "food-harvest-firestore",
   storageBucket: "food-harvest-firestore.appspot.com",
   messagingSenderId: "60579697874",
   appId: "1:60579697874:web:8f684e8b74b111d7e2c90b",
-  measurementId: "G-4XN183KB6Y"
+  measurementId: "G-4XN183KB6Y",
 };
-const app = firebase.initializeApp(config);
 
-export const auth = app.auth();
-export default firebase.firestore();
+// backup db
+const backupConfig = {
+  apiKey: "AIzaSyAeYJOOJbUDgnh_m86WKphuY5ntAilt8k8",
+  authDomain: "food-harvester-firebase-backup.firebaseapp.com",
+  projectId: "food-harvester-firebase-backup",
+  storageBucket: "food-harvester-firebase-backup.appspot.com",
+  messagingSenderId: "860549266983",
+  appId: "1:860549266983:web:4cbe3cf5dc1b45d6ffd802",
+};
+
+firebase.initializeApp(backupConfig);
+
+let firestore = firebase.firestore();
+
+export default firestore;
