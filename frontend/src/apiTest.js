ad810a83ce140690afa52c851a58a347d0e78bb9
snabelA00 <ja000411@gmail.com>
2021-09-30 17:08:04 +0200

    Update apiTest.js

--- frontend/src/apiTest.js
+++ frontend/src/apiTest.js
@@ -1,4 +1,14 @@
 
+// import _ from 'lodash';
+
+// export const requestQuotes = _.memoize(async title => {
+//     const res = await fetch(`https://animechan.vercel.app/api/quotes/anime?title=${title}`)
+//     if(res.status !== 200) return [];
+
+//     const quotesArray = await res.json();
+//     return quotesArray;
+// });
+
 import _ from 'lodash';
 
 export const requestQuotes = _.memoize(async title => {
