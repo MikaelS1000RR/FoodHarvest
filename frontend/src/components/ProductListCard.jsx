65c2bd3cf3824d85bf66445012f7aca012806640
TiredBirb <72065138+anna636@users.noreply.github.com>
2021-09-30 11:29:04 +0200

    Making css

--- frontend/src/components/ProductListCard.jsx
+++ frontend/src/components/ProductListCard.jsx
@@ -3,7 +3,7 @@ import { Button } from "reactstrap"
 const ProductListCard = () =>{
   return(
     <>
-      <Button>Click me!</Button>
+      
   </>
   )
 }
--- frontend/src/pages/MyProductLists.jsx
+++ frontend/src/pages/MyProductLists.jsx
@@ -1,9 +1,30 @@
+import { Row } from "reactstrap";
 import ProductListCard from "../components/ProductListCard"
+
+
 
 const MyProductLists = () => {
   return (
     <div className="container" style={styles.container}>
       <div className="listsContainer" style={styles.listsContainer}>
+        <div className="plusAndP" style={styles.plusAndP}>
+          
+            <p className="sparadeListor" style={styles.sparadeListor}>
+              Sparade listor
+            </p>
+          
+         
+            <img
+              className="plusImg"
+              style={styles.plusImg}
+              src="https://cdn-icons-png.flaticon.com/512/1828/1828817.png"
+            />
+          
+
+
+          
+        </div>
+
         <ProductListCard />
       </div>
     </div>
@@ -14,13 +35,30 @@ export default MyProductLists;
 
 const styles = {
   container: {
-   height:"70vh",
-    background: "pink",
-    marginTop:"8vh",
+    height: "70vh",
+
+    marginTop: "8vh",
   },
   listsContainer: {
-    /* minWidth: "100vw",
+    //background: "pink",
+    height: "100%",
+    display: "flex",
+    flexDirection: "Column",
+    textAlign: "center",
+  },
 
-    height: "60vh", */
+  sparadeListor: {
+    fontSize: "1.25rem",
+    fontWeight: "bold",
+  },
+
+  plusImg: {
+    height:"6vh"
+  },
+  plusAndP: {
+    display: "flex",
+    flexDirection: "Row",
+    margin: "0",
+    padding:"0"
   },
 };
