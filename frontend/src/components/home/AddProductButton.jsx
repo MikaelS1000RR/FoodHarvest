ea59d88d30b59f4b4f12e020ba6d3715951c46e7
Qiling <professorqtaku@gmail.com>
2021-10-01 10:36:20 +0200

    add button toggle login and addList modal if non of them exist

--- frontend/src/components/home/AddProductButton.jsx
+++ frontend/src/components/home/AddProductButton.jsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
+import { Button } from "reactstrap";
+import { useAuth } from "../../contexts/AuthContext";
+import { useModal } from "../../contexts/ModalContext";
 import { useProductList } from "../../contexts/ProductListContext";
 
 const AddProductButton = (props) => {
   const { buttonText, product } = props;
-  const { addProductToCurrentList } = useProductList();
+  const [ isInProductList, setIsInProductList] = useState(false)
+  const { currentProductList, addProductToCurrentList } = useProductList();
+  const { toggleAddListModal, toggleLoginModal } = useModal();
+  const { currentUser } = useAuth();
 
   const addProduct = () => {
+    if (!currentUser) {
+      toggleLoginModal();
+      return
+    }
+    else if (!currentProductList) {
+      toggleAddListModal();
+      return
+    }
+
     let isSucceed = addProductToCurrentList(product);
     if (isSucceed) {
-      
+      setIsInProductList(!isInProductList)
     }
   }
 
   return (
-      <div className="btn btn-primary" style={styles.button} onClick={addProduct}>
+      <Button style={styles.button} onClick={addProduct} disabled={isInProductList}>
         {buttonText}
-      </div>
+      </Button>
   );
 }
  
