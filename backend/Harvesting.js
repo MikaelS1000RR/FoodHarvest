46f573bafae84c4fea4b101ca58b460960549806
Qiling <professorqtaku@gmail.com>
2021-10-05 14:18:45 +0200

    Revert "remove comment and move SearchBar"
    This reverts commit 3e9d8d43c7e02c66e9150ddc71975bff0d39577d.

--- backend/Harvesting.js
+++ backend/Harvesting.js
@@ -7,13 +7,29 @@ import { WillysHandler } from "./Handlers/WillysHandler.js";
 
 export class Harvesting {
   static async run() {
+
+
+     function writeToFile(fileName, data) {
+       fs.writeFileSync(fileName, JSON.stringify(data, null, "  "), "utf-8");
+     }
+    // //Here we get all categories from Mathem
+    // let categories = await MathemHarvester.getCategories();
+
+
+
+    // //Here we use all categories to get products from each category
+    // let allProductsOfMathem = await MathemHarvester.getProductsFromCategories(categories)
+
+
+    // let scrubbedProductsOfMathem=await MathemScrubber.scrubAll(allProductsOfMathem)
+    // writeToFile('scrubbed-mathem.json', scrubbedProductsOfMathem)
     
-    function writeToFile(fileName, data) {
-      fs.writeFileSync(fileName, JSON.stringify(data, null, "  "), "utf-8");
-    }
+    //Deleting collection before posting anything
+   /*  await FirebaseHandler.deleteCollection("products");
 
     WillysHandler.run();
     HemkopHandler.run();
+    */
     MathemHandler.run();
   }
 
