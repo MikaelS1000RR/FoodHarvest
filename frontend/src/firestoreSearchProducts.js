60ba03928cf1b75c3b5dc67ee6481c995db4ce1e
snabelA00 <ja000411@gmail.com>
2021-10-05 10:57:51 +0200

    added trim() to search-input-string

--- frontend/src/firestoreSearchProducts.js
+++ frontend/src/firestoreSearchProducts.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
 import firestore from "./database_config/firestore";
 
-export const requestQuotes = _.memoize(async product => {
-    console.log(product)
+export const requestQuotes = _.memoize(async productNameSearch => {
+    console.log(productNameSearch)
 
     const docs = [];
     let snapshot = await firestore
       .collection("products")
-      .where("productName", ">=", capitalizeTheFirstLetters(product))
-      .where("productName", "<", endString(product))
+      .where("productName", ">=", adaptStringToDbData(productNameSearch))
+      .where("productName", "<", endCodeString(productNameSearch))
       .limit(15)
       .get();
     
@@ -21,20 +21,27 @@ export const requestQuotes = _.memoize(async product => {
     
 });
 
-function capitalizeTheFirstLetters(searchStr) {
-  const words = searchStr.split(" ");
+function adaptStringToDbData(searchStr) {
+  let startString = ""
+  const trimSearchStr = searchStr.trim()
+  const words = trimSearchStr.split(" ");
   for (let i = 0; i < words.length; i++) {
+      console.log("words[i]: ", words[i])
+      if(!words[i][0]){
+        startString = " "
+        return startString;
+      }
       words[i] = words[i][0].toUpperCase() + words[i].substr(1);
   }
 
-  let startString = words.join(" ");
+  startString = words.join(" ");
   console.log(startString)
   return startString
 }
 
-function endString(searchStr) {
-  let strSearch = capitalizeTheFirstLetters(searchStr)
-  let strlength = strSearch.length;
+function endCodeString(searchStr) {
+  let strSearch = adaptStringToDbData(searchStr)
+  let strlength = adaptStringToDbData(searchStr).length;
   let strFrontCode = strSearch.slice(0, strlength-1);
   let strEndCode = strSearch.slice(strlength-1, strSearch.length);
   let endCode = strFrontCode + String.fromCharCode(strEndCode.charCodeAt(0) + 1);
