3e9d8d43c7e02c66e9150ddc71975bff0d39577d
Qiling <professorqtaku@gmail.com>
2021-10-05 14:03:46 +0200

    remove comment and move SearchBar
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>

--- backend/Harvesting.js
+++ backend/Harvesting.js
@@ -7,29 +7,13 @@ import { WillysHandler } from "./Handlers/WillysHandler.js";
 
 export class Harvesting {
   static async run() {
-
-
-     function writeToFile(fileName, data) {
-       fs.writeFileSync(fileName, JSON.stringify(data, null, "  "), "utf-8");
-     }
-    // //Here we get all categories from Mathem
-    // let categories = await MathemHarvester.getCategories();
-
-
-
-    // //Here we use all categories to get products from each category
-    // let allProductsOfMathem = await MathemHarvester.getProductsFromCategories(categories)
-
-
-    // let scrubbedProductsOfMathem=await MathemScrubber.scrubAll(allProductsOfMathem)
-    // writeToFile('scrubbed-mathem.json', scrubbedProductsOfMathem)
     
-    //Deleting collection before posting anything
-   /*  await FirebaseHandler.deleteCollection("products");
+    function writeToFile(fileName, data) {
+      fs.writeFileSync(fileName, JSON.stringify(data, null, "  "), "utf-8");
+    }
 
     WillysHandler.run();
     HemkopHandler.run();
-    */
     MathemHandler.run();
   }
 
