2e84da7794ded080faf910c1fa556f5706b486c4
Qiling <professorqtaku@gmail.com>
2021-10-05 15:37:46 +0200

    add cart to navbar
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>
    Co-Authored-By: Johanna Andersson <72018736+snabelA2000@users.noreply.github.com>

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -10,6 +10,7 @@ import {
 import GuestNav from './GuestNav';
 import UserNav from './UserNav';
 import ProductListDropdown from './ProductListDropdown';
+import FavoriteCart from './FavoriteCart'
 import { useAuth } from '../../contexts/AuthContext';
 
 const Navigationbar = (props) => {
@@ -21,7 +22,10 @@ const Navigationbar = (props) => {
     <div>
       <Navbar color="primary" light expand="md">
         <Nav className="mr-auto" navbar>
-          <ProductListDropdown setMenu={setHamburgerMenu}/>
+          <ProductListDropdown setMenu={setHamburgerMenu} />
+        </Nav>
+        <Nav className="mr-auto" navbar>
+          <FavoriteCart setMenu={setHamburgerMenu} />
         </Nav>
 
         <Link to="/" style={styles.link}>
@@ -32,10 +36,11 @@ const Navigationbar = (props) => {
 
         <Collapse isOpen={hamburgerMenu} navbar>
           <Nav className="mr-auto" navbar style={styles.nav}>
-            {currentUser
-              ? <UserNav styles={styles} />
-              : <GuestNav styles={styles} />
-            }
+            {currentUser ? (
+              <UserNav styles={styles} />
+            ) : (
+              <GuestNav styles={styles} />
+            )}
           </Nav>
         </Collapse>
       </Navbar>
