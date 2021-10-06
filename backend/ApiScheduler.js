1054dd71b1748742d2997e2f227ef08ec8214b6b
Jonte111 <jonathan.pe@telia.com>
2021-10-06 14:54:08 +0200

    Tested ApiScheduler and imported the function to app.js.

--- backend/ApiScheduler.js
+++ backend/ApiScheduler.js
@@ -1,5 +1,11 @@
 import scheduler from 'node-schedule';
 
-const dailyJob = scheduler.scheduleJob('0 0 8 * * *', () => {
-  console.log("Test av scheduler")
-})
+export class ApiScheduler{
+  static run() {
+    const dailyJob = scheduler.scheduleJob('0 0 8 * * *', () => {
+      console.log("Starting scheduler");
+        Harvesting.run();
+    })
+  }
+
+}
