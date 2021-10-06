5d2e4e3266b1bd5bc3d2cbd9d4d4b0cf7a143243
MikaelS1000RR <mikaelsdr@icloud.com>
2021-10-06 11:24:46 +0200

    .

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -28,7 +28,6 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       setCurrentUser(user);
-      console.log("logged in as ", user.uid)
      
     })
     return unsubscribe;
