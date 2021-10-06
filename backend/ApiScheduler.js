b307248e0274daba403d3cd58ab69adf0d7433ba
Jonte111 <jonathan.pe@telia.com>
2021-10-06 11:53:29 +0200

    Update ApiScheduler.js

--- backend/ApiScheduler.js
+++ backend/ApiScheduler.js
@@ -5,5 +5,6 @@ const ruleScheduler = new scheduler.RecurrenceRule();
 ruleScheduler.hour = 00;
 ruleScheduler.minute = 00;
 ruleScheduler.second = 00;
-//0 = monday etc
+
+//From 0 (monday) to 6 (sunday)
 ruleScheduler.dayOfWeek = new scheduler.Range(0, 6);
