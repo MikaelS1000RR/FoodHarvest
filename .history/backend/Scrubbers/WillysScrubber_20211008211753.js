7752593bc367baa50a17bffdbb81a8660903f61b
MikaelS1000RR <mikaelsdr@icloud.com>
2021-10-08 21:21:37 +0200

    Cleanup
    Co-Authored-By: Qiling <72065263+professorqtaku@users.noreply.github.com>
    Co-Authored-By: Jonte111 <72066499+Jonte111@users.noreply.github.com>

--- .history/backend/Scrubbers/WillysScrubber_20211008211753.js
+++ .history/backend/Scrubbers/WillysScrubber_20211008211753.js
@@ -1,6 +1,6 @@
-
+import fetch from 'node-fetch'
 import { Scrubber } from './Scrubber.js'
-
+import { WillysHarvester } from '../Harvesters/WillysHarvester.js'
 import { FirebaseHandler } from "../FirebaseHandler.js";
 
 import { Preference } from "../Models/Preference.js"
