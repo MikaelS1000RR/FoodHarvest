2988384d9aa60c095cbd26b84b083a33a99a83d8
Qiling <professorqtaku@gmail.com>
2021-10-08 21:17:52 +0200

    remove unused import

--- backend/Scrubbers/WillysScrubber.js
+++ backend/Scrubbers/WillysScrubber.js
@@ -1,10 +1,6 @@
-import fetch from 'node-fetch'
 import { Scrubber } from './Scrubber.js'
-import { WillysHarvester } from '../Harvesters/WillysHarvester.js'
 import { FirebaseHandler } from "../FirebaseHandler.js";
-import { Store } from '../Models/Store.js'
 import { Preference } from "../Models/Preference.js"
-import { Discount } from '../Models/Discount.js';
 
 export class WillysScrubber extends Scrubber {
   static translateSchema = {
